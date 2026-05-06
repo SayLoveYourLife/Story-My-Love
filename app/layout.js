a4f919e91a7f6c49bfcaf22fac4cf2d5825fd9c5
SayLoveYourLife <aroslavstarutsyns@gmail.com>
2026-05-06 18:49:48 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Elina",
+  description: "A love story",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
